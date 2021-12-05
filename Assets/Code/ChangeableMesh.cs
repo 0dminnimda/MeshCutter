89a5cdc3fdd50e8167b1a161e0a3c36a63666dd7
0dminnimda <0dminnimda@gmail.com>
2021-12-05 14:08:59 +0300

    Create ChangeableMesh.cs

---
+++ Assets/Code/ChangeableMesh.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeableMesh
+{
+    public List<Vector3> vertices;
+    public List<int> triangles;
+    public List<Vector3> normals;
+
+    public ChangeableMesh() : this(new List<Vector3>(), new List<int>(), new List<Vector3>())
+    {
+    }
+
+    public ChangeableMesh(List<Vector3> vertices, List<int> triangles, List<Vector3> normals)
+    {
+        this.vertices = vertices;
+        this.triangles = triangles;
+        this.normals = normals;
+    }
+}
