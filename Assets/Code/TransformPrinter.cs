8227bafd8a1bb61b7987df2607328cf66c96db6c
0dminnimda <0dminnimda@gmail.com>
2024-10-24 18:40:07 +0300

    Commit the rest of Assets/Code

---
+++ Assets/Code/TransformPrinter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformPrinter : MonoBehaviour
+{
+    [SerializeField]
+    Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("forward: " + t.forward.ToString() + " right: " + t.right.ToString() + " up: " + t.up.ToString());
+        Debug.Log(t.parent);
+    }
+}
